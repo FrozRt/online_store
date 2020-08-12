f5ee742d00f532a52687cf4032cf855e507b34e7
Mikhail Chigrin <chigrin_m@mail.ru>
2020-08-12 23:39:13 +0300

    Add small fixes

--- online_store/urls.py
+++ online_store/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('customer/', include('customer.urls')),
     path('coupons/', include('coupons.urls', namespace='coupons')),
     path('orders/', include('orders.urls', namespace='orders')),
     path('cart/', include('cart.urls', namespace='cart')),
--- store/recommender.py
+++ store/recommender.py
@@ -1,10 +1,11 @@
 import redis
 from django.conf import settings
-
 from .models import Product
 
 # connect to redis
-r = redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB)
+r = redis.StrictRedis(host=settings.REDIS_HOST,
+                      port=settings.REDIS_PORT,
+                      db=settings.REDIS_DB)
 
 
 class Recommender(object):
@@ -19,7 +20,9 @@ class Recommender(object):
                 # get the other products bought with each product
                 if product_id != with_id:
                     # increment score for product purchased together
-                    r.zincrby(self.get_product_key(product_id), with_id, amount=1)
+                    r.zincrby(self.get_product_key(product_id),
+                              with_id,
+                              amount=1)
 
     def suggest_products_for(self, products, max_results=6):
         product_ids = [p.id for p in products]
